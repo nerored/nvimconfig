b85c8c7d6b57a59e8e44f3b49c1a5a3d25a1756f
nerored <nero_stellar@icloud.com>
2025-09-04 15:33:12 +0800

    自定义插件

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,5 @@
 require "nvchad.autocmds"
+
+--local autocmd = vim.api.nvim_create_autocmd
+
+-- auto format
--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,24 +1,94 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
+    theme = "monekai",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+    -- hl_override = {
+    -- 	Comment = { italic = true },
+    -- 	["@comment"] = { italic = true },
+    -- },
 }
 
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
--- }
+M.nvdash = {
+    load_on_startup = true,
+    header = {
+        "⠀⠀⠀⠀⠀⠀⠀⠀⣴⡿⠛⠻⣿⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣶⣿⠉⠘⣿⣄⠀⠀⠀⠀⠀⠀",
+        "⠀⠀⠀⠀⠀⠀⠀⠀⣿⠃⠀⢀⡀⠀⠙⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⡀⣿⣿⣷⣤⣤⣤⣀⠀⣠⣶⠟⠉⠀⠀⠀⠀⣤⠀⠀⢻⣷⠀⠀⠀⠀⠀",
+        "⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀⣿⣿⠀⠀⠀⢻⣷⠀⠀⠀⠀⠀⠀⣴⣷⣱⣷⣥⣠⣽⣷⣥⡀⣀⠈⠙⣿⣿⣿⣿⣿⡍⠉⣷⣤⣀⠀⠀⢀⣿⠀⠀⠀⢻⣧⠀⠀⠀⠀",
+        "⠀⠀⠀⠀⠀⠀⠀⢸⡏⠀⠀⢹⣿⠀⠀⠀⢀⣴⣿⣽⡀⢀⣤⣷⣿⡿⢛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣀⠉⢷⡘⠛⠀⠀⠀⢸⣿⠀⠀⠀⠀",
+        "⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⣠⣶⣿⣠⣩⣿⣿⣿⣿⡿⣿⣹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣁⣿⡄⠀⠀⢸⣿⠀⠀⠀⠀",
+        "⠀⠀⠀⠀⠀⠀⠀⣿⠇⠀⣠⣿⣿⠁⠉⣽⣿⣿⣿⣿⣿⣿⣿⣿⡏⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⣍⠀⣝⣤⠸⣿⠀⠀⠀⠀",
+        "⠀⠀⠀⠀⠀⠀⠀⢿⡇⣿⠙⠙⢛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢻⠛⠛⠉⠙⠉⠉⠉⠉⠉⠉⠛⣿⣿⣿⣿⣿⣿⠛⠿⣿⣿⣿⢿⣿⡀⠀⣿⣦⠀⠀⠀⠀",
+        "⠀⠀⠀⠀⠀⠀⠀⠉⠉⢹⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⣿⢿⣿⣿⣿⣿⣻⣿⣿⣿⡟⣿⣿⣷⠙⣿⣦⠀⠀",
+        "⠀⠀⠀⠀⠀⠀⢐⣝⢹⣿⣿⣿⣿⠛⠛⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀",
+        "⠀⠀⠀⢘⢙⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣴⠶⠷⠶⠶⣦⣄⠀⠀⠀⠀⠀⠀⣀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇",
+        "⠀⢠⣽⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⡄⢀⣾⠋⠀⠀⠀⠙⠛⠻⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇",
+        "⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠷⠀⠀⠀⠀⠀⠀⣤⣤⡀⠀⠙⣿⠀⠀⠀⠀⠀⠀⠀⢀⣤⣄⠀⠙⡝⠛⣿⣿⣿⣿⣿⡟⠃",
+        "⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢻⠟⠻⠟⠁⠀⠀⠀⠀⠀⢸⣿⣿⣿⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡗⠀⣿⠀⠉⠙⣿⣿⣿⠟⠀",
+        "⠙⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠉⠸⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⢸⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⠀⠀⠀⠉⣿⠋⠀⠀",
+        "⠈⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠛⠉⠀⠀⠀⢻⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⠟⠹⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⠁⠀⠀⠀⠀⣿⠀⠀⠀",
+        "⠀⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⠿⢿⣿⣿⣿⣶⣶⣤⣤⣤⣄⣉⢷⣤⣀⣀⣀⣀⣠⣤⡶⠚⠉⠀⠀⠀⠀⠙⢶⣤⣄⣀⣤⣤⣴⣿⣯⣶⣶⣶⣶⣿⡛⠻⠋⠀⠀",
+        "⠀⠈⠈⠛⠿⣿⣿⣿⡇⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣦⣀⣀⣭⣭⣿⣿⣾⠁⠀⠀⠀⠀⠀⠀⣶⠀⠀⠀⠀⠀⢹⣶⣶⣤⣤⣤⣿⣿⣿⣿⣿⣿⣶⡄⠀⠀",
+        "⠀⠀⠀⠀⠀⠀⠈⠻⣇⢀⣀⣤⣤⣶⣿⣿⠿⠿⠛⠛⠙⠙⠙⢙⣆⣤⡶⠛⠉⢿⣷⠀⠀⠀⠀⠀⣀⣴⠛⣦⣀⠀⠀⣠⡾⠀⠀⠙⣾⣿⢛⡙⠛⠛⠻⢿⣿⣷⠀⠀",
+        "⠀⠀⠀⠀⠀⠴⠛⠛⣹⡅⠀⠀⠀⣿⣿⣿⣿⣿⣿⣽⣿⣿⠿⠛⠃⠀⠀⠀⠀⠀⠀⠉⠉⠙⠉⠉⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⣿⣽⠛⢿⣿⡍⣿⣿⡿⠀⠀⠀",
+        "⠀⠀⠀⠀⣤⠒⠉⠉⠘⣿⡀⠀⠀⠈⣿⣽⣿⡿⠟⠙⠙⣿⣿⠿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣙⠿⣿⠁⠀⠀⠀",
+        "⠀⢀⠖⠉⣧⠀⠀⠀⠀⣷⠙⣦⣤⡶⠛⠉⠉⠛⠛⠛⠛⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⢀⡾⠁⠈⠛⣤⠀",
+        "⣴⠁⠀⠀⠀⢳⠀⠀⠀⣼⣿⠿⠻⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠉⠀⠀⠀⠀⠀⠀",
+        "⣤⣄⠀⠀⠀⠀⠙⣶⠛⠀⠀⠳⣄⠀⢙⣶⣤⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣾⣿⣦⣶⣦⠀⠀⠀⠀⠀⠀",
+        "⠀⠀⠀⠙⣶⡟⠁⠈⢳⠀⠀⠀⠀⡽⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣶⣤⣀⣀⣴⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀",
+        "⠀⠀⢀⠟⠀⠈⣦⠀⠀⠙⣄⠀⣿⣿⣿⣿⣦⠛⣿⣿⣿⣿⣿⣏⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⣥⣶⣿⡀⠀⠀⠀⠀⠀⠀",
+        "⠀⢠⠋⠀⠀⠀⠘⡄⠀⠀⠈⣿⣿⣿⣿⣿⣿⣷⢻⣿⣿⣿⣿⣿⣿⣿⣶⣭⣭⣛⣛⠻⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⣋⣴⣾⠋⣾⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀",
+        "⣆⡏⠀⠀⠀⠀⠀⡇⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣍⠻⠟⣵⣿⣿⣿⣿⠏⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀",
+        "                                                            ",
+        "                     Powered By  faclon                   ",
+        "                                                            ",
+    },
+
+    buttons = {
+        { txt = "  查找文件", keys = "ff", cmd = "Telescope find_files" },
+        { txt = "  重新打开", keys = "fo", cmd = "Telescope oldfiles" },
+        { txt = "󰈭  查找内容", keys = "fw", cmd = "Telescope live_grep" },
+        { txt = "󱥚  主题设定", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
+        { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
+
+        { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true, align = "left" },
+
+        {
+            txt = function()
+                local stats = require("lazy").stats()
+                local ms = math.floor(stats.startuptime) .. " ms"
+                return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+            end,
+            hl = "NvDashFooter",
+            no_gap = true,
+        },
+
+        { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true, align = "left" },
+    },
+}
+M.ui = {
+    tabufline = {
+        lazyload = false,
+    },
+
+    cmp = {
+        icons_left = true,
+        style = "atom_colored",
+        format_colors = { lsp = true, icon = "󱓻" },
+    },
+
+    telescope = { style = "bordered" },
+
+    statusline = {
+        enabled = true,
+        theme = "minimal",
+        separator_style = "block",
+    },
+}
 
 return M
