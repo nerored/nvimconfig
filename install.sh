0954fc4f2ec97db30654908135d4a0147edabb5c
nerored <nero_stellar@icloud.com>
2025-09-04 15:33:12 +0800

    自定义插件
    save

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+git clone https://github.com/nerored/nvimconfig.git ~/.config/nvim && nvim
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "githubcopilot", "gopls", "buf_ls", "jsonls", "bashls" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
+vim.lsp.config["githubcopilot"] = {}
